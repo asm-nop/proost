e82932644c6c7f869ebd8e91521d801718efa225
0xKitsune <0xKitsune@protonmail.com>
2024-08-10 13:40:35 -0400

    example

--- mini-proost/examples/mini-proost.rs
+++ mini-proost/examples/mini-proost.rs
@@ -2,7 +2,7 @@ use std::{error::Error, path::PathBuf};
 use mini_proost::process_input;
  fn main()  {
 
-    let input = "def add : Nat -> Nat := λ x, x + 1";
+    let input = "def x: Prop := True";
     let res = process_input(input, PathBuf::new()).expect("Bad");
 
 }
