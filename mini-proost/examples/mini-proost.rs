35ad0126e7b8dc359e5a101d71dc926d32846c1c
0xKitsune <0xKitsune@protonmail.com>
2024-08-10 13:35:59 -0400

    update examples

--- mini-proost/examples/mini-proost.rs
+++ mini-proost/examples/mini-proost.rs
@@ -1,8 +1,8 @@
-use std::error::Error;
+use std::{error::Error, path::PathBuf};
 use mini_proost::process_input;
  fn main()  {
 
-    let input = "1 + 1";
-    let res = process_input(input).expect("Bad");
+    let input = "def add : Nat -> Nat := λ x, x + 1";
+    let res = process_input(input, PathBuf::new()).expect("Bad");
 
 }
